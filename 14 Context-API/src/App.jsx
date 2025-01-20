2f40fac0de466c6ca424013a1edf748de1134ccd
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-20 15:06:02 +0530

    including context for todo items and welcome message

---
+++ 14 Context-API/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import WelcomeMessage from "./components/WelcomeMessage";
+import "./App.css";
+import { TodoItemsContext } from "./store/todo-items-store";
+
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const addNewItem = (itemName, itemDueDate) => {
+    setTodoItems((currValue) => [
+      ...currValue,
+      { name: itemName, dueDate: itemDueDate },
+    ]);
+  };
+
+  const deleteItem = (name) => {
+    setTodoItems(
+      todoItems.filter((item) => {
+        return item.name !== name;
+      })
+    );
+  };
+
+  return (
+    <TodoItemsContext.Provider
+      value={{
+        todoItems,
+        addNewItem,
+        deleteItem,
+      }}
+    >
+      <center className="todo-container">
+        <AppName />
+        <AddTodo />
+        <WelcomeMessage />
+        <TodoItems key={todoItems}></TodoItems>
+      </center>
+    </TodoItemsContext.Provider>
+  );
+}
+
+export default App;
