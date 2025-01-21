436fa3405c1577c4870238c7c083eb63879731f1
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-21 14:46:42 +0530

    Add React Router for navigation and use useNavigate hook

--- 19 react-router/src/routes/App.jsx
+++ 19 react-router/src/routes/App.jsx
@@ -1,12 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Sidebar from "./components/Sidebar";
-import CreatePost from "./components/CreatePost";
-import PostList from "./components/PostList";
-import PostListProvider from "./store/post-list-store";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Sidebar from "../components/Sidebar";
+import PostListProvider from "../store/post-list-store";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [selectedTab, setSelectedTab] = useState("Home");
@@ -17,7 +16,7 @@ function App() {
         <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
         <div className="content">
           <Header />
-          {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+          <Outlet />
           <Footer />
         </div>
       </div>
