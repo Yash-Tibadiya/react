2b57c179f6123634c13c39a7cb4521de66717f45
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-21 11:39:27 +0530

    learned about useEffect for data fetching and remove unnecessary state management

--- 17 advance-react/src/components/PostList.jsx
+++ 17 advance-react/src/components/PostList.jsx
@@ -1,26 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
-import { useState } from "react";
 
 const PostList = () => {
   const { postList, addInitialPost } = useContext(PostListData);
-  const [dataFetched, setDataFetched] = useState(false);
 
-  if (!dataFetched && postList.length === 0) {
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => addInitialPost(data.posts));
-
-    setDataFetched(true);
-  }
+  },[]);
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage />
-      )}
+      {postList.length === 0 && <WelcomeMessage />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
