9a78bcb3a451faac655f55cd520a1b661b8c73f6
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-22 12:45:21 +0530

    Refactor application structure by implementing routing and adding Header, Footer, Home, and Bag components

--- 22 Myntra Clone/3 myntra-clone/src/main.jsx
+++ 22 Myntra Clone/3 myntra-clone/src/main.jsx
@@ -1,10 +1,32 @@
+import "./index.css";
+import App from "./routes/App.jsx";
+import Bag from "./routes/Bag.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./routes/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        // loader: postLoader,
+      },
+      {
+        path: "/bag",
+        element: <Bag />,
+        // action: createPostAction,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
